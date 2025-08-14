339a0e5f10d4841f0f06973d2c4c648f62fc4f4e
Danner Kurtz <kurtz.danner@gmail.com>
2025-08-13 23:22:07 -0300

    feat: add cart summery

--- src/app/common/cart.tsx
+++ src/app/common/cart.tsx
@@ -2,6 +2,8 @@
 
 import { getCart } from "@/actions/get-cart";
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
 import {
 	Sheet,
 	SheetContent,
@@ -9,6 +11,7 @@ import {
 	SheetTitle,
 	SheetTrigger,
 } from "@/components/ui/sheet";
+import { formatCentsToBRL } from "@/helpers/money";
 import { useQuery } from "@tanstack/react-query";
 import { ShoppingBasketIcon } from "lucide-react";
 import CartItem from "./cart-item";
@@ -29,19 +32,48 @@ const Cart = () => {
 				<SheetHeader>
 					<SheetTitle>Carrinho</SheetTitle>
 				</SheetHeader>
-				<div className="space-y-4 px-5 ">
-					{cartIsPending && <p>Loading...</p>}
-					{cart?.items.map((item) => (
-						<CartItem
-							key={item.id}
-							id={item.id}
-							productName={item.productVariant.product.name}
-							productVariantName={item.productVariant.name}
-							productVariantImageUrl={item.productVariant.imageUrl}
-							productVariantPriceInCents={item.productVariant.priceInCents}
-							quantity={item.quantity}
-						/>
-					))}
+				<div className="flex h-full flex-col">
+					<div className="flex-1 px-5">
+						<ScrollArea className="h-full">
+							<div className="flex flex-col gap-8">
+								{cart?.items.map((item) => (
+									<CartItem
+										key={item.id}
+										id={item.id}
+										productName={item.productVariant.product.name}
+										productVariantName={item.productVariant.name}
+										productVariantImageUrl={item.productVariant.imageUrl}
+										productVariantPriceInCents={
+											item.productVariant.priceInCents
+										}
+										quantity={item.quantity}
+									/>
+								))}
+							</div>
+						</ScrollArea>
+					</div>
+					{cart?.items && cart?.items.length > 0 && (
+						<div className="mt-auto p-5">
+							<Separator className="my-4" />
+							<div className="flex items-center justify-between text-xs font-medium">
+								<p>Subtotal</p>
+								<p>{formatCentsToBRL(cart?.totalInCents ?? 0)}</p>
+							</div>
+							<Separator className="my-4" />
+							<div className="flex items-center justify-between text-xs font-medium">
+								<p>Entrega: </p>
+								<p>Grátis</p>
+							</div>
+							<Separator className="my-4" />
+							<div className="flex items-center justify-between text-xs font-medium">
+								<p>Total</p>
+								<p>{formatCentsToBRL(cart?.totalInCents ?? 0)}</p>
+							</div>
+							<Separator className="my-4" />
+
+							<Button className="w-full rounded-full">Finalizar pedido</Button>
+						</div>
+					)}
 				</div>
 			</SheetContent>
 		</Sheet>
